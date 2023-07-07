a85fccf5ccd01e2760b15dade2abea025abbc83d
Kubra KIRAS <kbr.aslan.k@gmail.com>
2023-07-07 15:44:43 +0300

    ignored

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/kubra/Desktop/test/learning-nuxt/node_modules/.pnpm/@nuxt+vite-builder@3.6.2_@types+node@18.0.0_vue@3.3.4/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
