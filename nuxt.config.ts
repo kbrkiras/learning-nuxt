d884e04a12ab9245e62f94ecf32df72480abfa6c
Kubra KIRAS <kbr.aslan.k@gmail.com>
2023-07-07 16:26:28 +0300

    Dynamic sidebar added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindForms from '@tailwindcss/forms';
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules:
+   [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon'
+   ],
   devtools: { enabled: true },
   css: [
     '@/assets/css/main.css',
